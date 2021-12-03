6f3999e60f06cd46a2b572012385e442eba00a40
cool7coding <76494513+cooldudeseven7@users.noreply.github.com>
2021-12-03 10:36:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ screen = turtle.Screen()
 ####SETTINGS####
 MANAGED = False #if the browser is managed
 MANAGED_BY = 'ENTER_HERE' #who/what orginizatoin the browser is managed by
-BLOCKED_SITES = ['SITE_HERE','SITE_HERE'] #sites that are bolcked. Feel free to add more
-####MAIN####
+BLOCKED_SITES = ['SITE_HERE','SITE_HERE'] #sites that are bolcked. Feel free to add more sites
+COMMANDLINE_ENABLED = True #If the user can use the CommandLine
+####MAIN [CODE]####
