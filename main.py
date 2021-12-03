9a8bc5e9a7721055d17c7eb57dad2e8f2a643a5f
cool7coding <76494513+cooldudeseven7@users.noreply.github.com>
2021-12-03 10:26:53 -0500

    Make Settings+File
    The Main configuratoin - pre-complete

---
+++ main.py
@@ -0,0 +1,8 @@
+from tkinter import messagebox
+import turtle
+screen = turtle.Screen()
+####SETTINGS####
+MANAGED = False #if the browser is managed
+MANAGED_BY = 'ENTER_HERE' #who/what orginizatoin the browser is managed by
+BLOCKED_SITES = ['SITE_HERE','SITE_HERE'] #sites that are bolcked. Feel free to add more
+####MAIN####
